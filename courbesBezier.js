97b1ac5088ccaaaec11ec1a4c428c5d10228cfd0
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2022-10-12 14:57:28 +0200

    courbe via bernstein

--- courbesBezier.js
+++ courbesBezier.js
@@ -4,5 +4,65 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.5, 1000);
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0, 1, 0);
-renderer.render(scene, camera);
+camera.position.set( 0, 0, 10 );
+camera.lookAt( 0, 0, 0 );
+
+scene.background = new THREE.Color(0, 0, 0);
+
+const material = new THREE.LineBasicMaterial({color: 0x1495A4});
+const material2 = new THREE.LineBasicMaterial({color: 0xFF0000});
+const materialPoints = new THREE.PointsMaterial({color: 0xFFFF00, size : 0.15});
+
+let geometry;
+let drawing;
+
+let pointsControle1 = new Array;
+pointsControle1.push(new THREE.Vector3(0,0,0));
+pointsControle1.push(new THREE.Vector3(0,1,0));
+pointsControle1.push(new THREE.Vector3(1,1,0));
+pointsControle1.push(new THREE.Vector3(1,0,0));
+
+let pointsControle2 = new Array;
+pointsControle2.push(new THREE.Vector3(0,0,0));
+pointsControle2.push(new THREE.Vector3(1,0,0));
+pointsControle2.push(new THREE.Vector3(0,1,0));
+pointsControle2.push(new THREE.Vector3(1,1,0));
+
+let pointsControle3 = new Array;
+pointsControle3.push(new THREE.Vector3(0,0,0));
+pointsControle3.push(new THREE.Vector3(1,1,0));
+pointsControle3.push(new THREE.Vector3(0,1,0));
+pointsControle3.push(new THREE.Vector3(1,0,0));
+
+let pointsControle = pointsControle3;
+
+const vertices = [];
+
+for ( let i = 0; i < pointsControle.length; i ++ ) {
+
+	const x = pointsControle[i].x;
+	const y = pointsControle[i].y;
+	const z = 0;
+
+	vertices.push( x, y, z );
+
+}
+
+let points = createBerstein(pointsControle);
+
+geometry = new THREE.BufferGeometry().setFromPoints(pointsControle);  // on affiche les points
+drawing = new THREE.Line(geometry, material2); // on relie les points grace à .Line de façon à dessiner la courbe paramétrique
+scene.add(drawing); // on ajoute le dessin dans la scène
+
+geometry = new THREE.BufferGeometry();  // on affiche les points
+geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+drawing = new THREE.Points(geometry, materialPoints); // on relie les points grace à .Line de façon à dessiner la courbe paramétrique
+scene.add(drawing); // on ajoute le dessin dans la scène
+
+geometry = new THREE.BufferGeometry().setFromPoints(points);  // on affiche les points
+drawing = new THREE.Line(geometry, material); // on relie les points grace à .Line de façon à dessiner la courbe paramétrique
+scene.add(drawing); // on ajoute le dessin dans la scène
+
+
+
+renderer.render(scene, camera); // on fait le rendu
---
+++ creationCourbeBerstein.js
@@ -0,0 +1,38 @@
+function Bernstein(n, i, t) {
+    return (fact(n)/(fact(i)*fact(n-i)))*Math.pow(t,i)*Math.pow((1-t), n-i);
+}
+
+function createBerstein(pointsControle) {
+    let x = 0;                         
+    let y = 0;
+    let taille = pointsControle.length;
+
+    if(taille>=3){
+        let courbe = [];
+
+        for(let t = 0; t<1;t=t+0.005){
+            x = 0;
+            y = 0;
+            for(let i = 0; i<taille;i++){
+                let Bern = Bernstein(taille-1, i, t);
+                x = x + pointsControle[i].x * Bern;
+                y = y + pointsControle[i].y * Bern;
+            }
+            courbe.push(new THREE.Vector3(x,y,0));
+        }        
+        
+        return courbe;
+    }
+    else {
+        return "error";
+    }
+}
+
+function fact(x){
+	let result = 1;
+	while (x>0){
+		result = result*x;
+		x--;
+	}
+	return result;
+}
