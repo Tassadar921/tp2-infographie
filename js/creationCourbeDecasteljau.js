299fe544015075abdfbfe06beb5868bf0835fa25
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2022-11-09 00:18:55 +0100

    commentaire

--- js/creationCourbeDecasteljau.js
+++ js/creationCourbeDecasteljau.js
@@ -1,6 +1,6 @@
-function Decasteljau(pointsControle, k, j, t, dim) {
-    if (k == 0) {
-        switch (dim) {
+function Decasteljau(pointsControle, k, j, t, dim) {//algorithme récursif de decasteljau
+    if (k == 0) {//condition de sortie : si k vaut 0 (on se trouve à la base de la pyramide)
+        switch (dim) {//permet juste de retourner la valeur de x ou y
             case "x":
                 return (pointsControle[j].x);
                 break
@@ -10,7 +10,7 @@ function Decasteljau(pointsControle, k, j, t, dim) {
         }
     }
     else {
-        return ( (1-t)*Decasteljau(pointsControle, k - 1, j, t, dim) + t*Decasteljau(pointsControle, k - 1, j + 1, t, dim) );
+        return ( (1-t)*Decasteljau(pointsControle, k - 1, j, t, dim) + t*Decasteljau(pointsControle, k - 1, j + 1, t, dim));//calcul des deux points précédant en appelant la fonction dans laquelle on se trouve
     }
 }
 
@@ -18,22 +18,23 @@ function createDecastlejau(pointsControle) {
     let x = 0;                         
     let y = 0;
     let taille = pointsControle.length;
+    //mise en place des variables utiles par la suite
 
-    if(taille>=3){
+    if(taille>=3){//si la courbe ne comporte pas 3 points ignore la suite
         let courbe = [];
-
-        for(let t = 0; t<1;t=t+0.005){
+        //création du tableau lié à la courbe
+        for(let t = 0; t<1;t=t+0.005){//le t variant de 0 à 1 dans l'équation de bernstein pour pouvoir créer chaque point
             x = 0;
             y = 0;
             for(let i = 0; i<taille;i++){
-                x = Decasteljau(pointsControle, pointsControle.length-1, 0, t, "x");
+                x = Decasteljau(pointsControle, pointsControle.length-1, 0, t, "x");//appel de l'agorythme récursif de decasteljau sur x et y
                 y = Decasteljau(pointsControle, pointsControle.length-1, 0, t, "y");
             }
-            courbe.push(new THREE.Vector3(x,y,0));
+            courbe.push(new THREE.Vector3(x,y,0));//ajout des points à la courbe
         }        
-        return courbe;
+        return courbe;//on retourne tous les points de la courbe
     }
     else {
-        return "error";
+        return "error";//retourne une erreur (utilisé pour du déboguage)
     }
 }
